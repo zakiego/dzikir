01d74b9114158c56aff7ad40d6d54a15f78a7074
Zakiego <belummikir@gmail.com>
2021-07-29 12:42:19 +0800

    Done API

---
+++ pages/dzikir-pagi.js
@@ -0,0 +1,44 @@
+export async function getServerSideProps() {
+	const res = await fetch(`http://${process.env.URL}/api/public/v1/getDzikir`);
+	const data = await res.json();
+
+	if (!data) {
+		return {
+			notFound: true,
+		};
+	}
+
+	return {
+		props: { data }, // will be passed to the page component as props
+	};
+}
+
+export default function Home(props) {
+	const jenisDzikir = "dzikir sore";
+
+	const {
+		data: {
+			data: { dzikir, sumber },
+		},
+	} = props;
+
+	const filteredDzikir = dzikir.filter(function (el) {
+		return el.category.includes(jenisDzikir);
+	});
+
+	const renderDzikir = filteredDzikir.map((dzikir) => {
+		return (
+			<li key={dzikir.id}>
+				<p>{dzikir.arabic}</p>
+				<p>{dzikir.arti}</p>
+			</li>
+		);
+	});
+
+	return (
+		<div>
+			<h1>Hai</h1>
+			<div>{renderDzikir}</div>
+		</div>
+	);
+}
