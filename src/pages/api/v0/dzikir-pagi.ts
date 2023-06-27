909f289bd77baef9dbf5328b17a79a8248caef61
zakiego <belummikir@gmail.com>
2023-06-27 15:25:34 +0800

    feat: add api

---
+++ src/pages/api/v0/dzikir-pagi.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import * as fs from "fs";
+import { keystaticSchema } from "~/src/utils/keystatic";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const data = keystaticSchema.dzikir
+    .pick({
+      dzikir: true,
+    })
+    .parse(JSON.parse(fs.readFileSync("content/dzikir-pagi.json", "utf-8")));
+
+  const addId = data.dzikir.map((item, index) => {
+    return {
+      id: index + 1,
+      ...item,
+    };
+  });
+
+  res.status(200).json(addId);
+};
+
+export default handler;
