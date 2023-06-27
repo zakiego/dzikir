510e2edb5a987cead379a1486c201ee38493cc9a
zakiego <belummikir@gmail.com>
2023-06-27 15:50:25 +0800

    refactor: dzikir schema

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,5 +1,24 @@
 import { config, fields, singleton } from "@keystatic/core";
 
+const dzikirSchema = {
+  title: fields.text({ label: "Title" }),
+  description: fields.text({ label: "Description" }),
+  dzikir: fields.array(
+    fields.object({
+      arabic: fields.text({ label: "Arabic", multiline: true }),
+      latin: fields.text({ label: "Latin", multiline: true }),
+      translation: fields.text({ label: "Translation", multiline: true }),
+      source: fields.text({ label: "Source", multiline: true }),
+      read: fields.text({ label: "Read", multiline: true }),
+      benefit: fields.text({ label: "Benefit", multiline: true }),
+    }),
+    {
+      itemLabel: (item) => item.fields.arabic.value,
+      label: "Dzikir",
+    },
+  ),
+};
+
 export const keystaticConfig = config({
   storage: {
     kind: "local",
@@ -28,47 +47,13 @@ export const keystaticConfig = config({
       label: "Dzikir Pagi",
       path: "public/content/dzikir-pagi",
       format: "json",
-      schema: {
-        title: fields.text({ label: "Title" }),
-        description: fields.text({ label: "Description" }),
-        dzikir: fields.array(
-          fields.object({
-            arabic: fields.text({ label: "Arabic", multiline: true }),
-            latin: fields.text({ label: "Latin", multiline: true }),
-            translation: fields.text({ label: "Translation", multiline: true }),
-            source: fields.text({ label: "Source", multiline: true }),
-            read: fields.text({ label: "Read", multiline: true }),
-            benefit: fields.text({ label: "Benefit", multiline: true }),
-          }),
-          {
-            itemLabel: (item) => item.fields.arabic.value,
-            label: "Dzikir",
-          },
-        ),
-      },
+      schema: dzikirSchema,
     }),
     dzikirSore: singleton({
       label: "Dzikir Sore",
       path: "public/content/dzikir-sore",
       format: "json",
-      schema: {
-        title: fields.text({ label: "Title" }),
-        description: fields.text({ label: "Description" }),
-        dzikir: fields.array(
-          fields.object({
-            arabic: fields.text({ label: "Arabic", multiline: true }),
-            latin: fields.text({ label: "Latin", multiline: true }),
-            translation: fields.text({ label: "Translation", multiline: true }),
-            source: fields.text({ label: "Source", multiline: true }),
-            read: fields.text({ label: "Read", multiline: true }),
-            benefit: fields.text({ label: "Benefit", multiline: true }),
-          }),
-          {
-            itemLabel: (item) => item.fields.arabic.value,
-            label: "Dzikir",
-          },
-        ),
-      },
+      schema: dzikirSchema,
     }),
   },
 });
